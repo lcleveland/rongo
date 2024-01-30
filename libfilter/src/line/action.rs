f60ab906b2924bd1d417578ad2bd0832dee86b13
Lyle Cleveland <lyle.cleveland@yahoo.com>
2024-01-30 09:12:01 -0600

    add action types

--- libfilter/src/line/action.rs
+++ libfilter/src/line/action.rs
@@ -1,14 +1,54 @@
+use crate::line::rgba::RGBA;
 #[derive(Debug)]
 pub enum Action {
-    SetBorderColor,
-    SetTextColor,
-    SetBackgroundColor,
-    SetFontSize,
-    PlayAlertSound,
-    PlayAlertSoundPositional,
+    SetBorderColor(RGBA),
+    SetTextColor(RGBA),
+    SetBackgroundColor(RGBA),
+    SetFontSize(u16),
+    PlayAlertSound((u8, u16)),
+    PlayAlertSoundPositional((u8, u16)),
     DisableDroupSound,
     EnableDropSound,
-    CustomAlertSound,
-    MinimapIcon,
-    PlayEffect,
+    CustomAlertSound(String),
+    MinimapIcon((Size, Color, Shape)),
+    PlayEffect((bool, Color, bool)),
+}
+
+#[derive(Debug)]
+pub enum Size {
+    Disable,
+    Small,
+    Medium,
+    Largest,
+}
+
+#[derive(Debug)]
+pub enum Color {
+    Red,
+    Green,
+    Blue,
+    Brown,
+    White,
+    Yellow,
+    Cyan,
+    Grey,
+    Orange,
+    Pink,
+    Purple,
+}
+
+#[derive(Debug)]
+pub enum Shape {
+    Circle,
+    Diamond,
+    Hexagon,
+    Square,
+    Star,
+    Triangle,
+    Cross,
+    Moon,
+    Raindrop,
+    Kite,
+    Pentagon,
+    UpsideDownHouse,
 }
