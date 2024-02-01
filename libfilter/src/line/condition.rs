79a00a3995b48f1b0db00f0c24964d6badfcc3b9
Lyle Cleveland <lyle.cleveland@yahoo.com>
2024-02-01 09:23:13 -0600

    more docs for condition

--- libfilter/src/line/condition.rs
+++ libfilter/src/line/condition.rs
@@ -37,25 +37,45 @@ pub enum Condition {
     HasExplicitMod(String),
     /// If an item has any enchantment from the Labyrinth.
     AnyEnchantment(bool),
+    /// Filter Cluster Jewels by enchantment type.
     EnchantmentPassiveNode(String),
+    /// Filter Cluster Jewels by the number of enchantments. Only checks the "Adds X passive skills" modifier.
     EnchantmentPassiveNum(EnchantmentPassiveNum),
+    /// Currency stack size.
     StackSize(StackSize),
+    /// The level of the gem.
     GemLevel(GemLevel),
+    /// The [GemQuality] of the gem.
     GemQualityType(GemQuality),
+    /// If an item has alternate quality or not.
     AlternativeQuality(bool),
+    /// If an item is an Replica or not.
     Replica(bool),
+    /// If an item is identified or not.
     Identified(bool),
+    /// If an item is corrupted or not.
     Corrupted(bool),
+    /// How many corrupted mods are present.
     CorruptedMods(CorruptedMods),
+    /// If an item is mirrored or not.
     Mirrored(bool),
+    /// If an item is an Elder Item or not.
     ElderItem(bool),
+    /// If an item is a Shaper Item or not.
     ShaperItem(bool),
+    /// If an item has an [Influence]
     HasInfluence(Influence),
+    /// If an item is fractured or not.
     FracturedItem(bool),
+    /// If an item is a Synthesised item or not
     SynthesisedItem(bool),
+    /// If the map is elder or not.
     ElderMap(bool),
+    /// If the map is Shaped or not.
     ShapedMap(bool),
+    /// If the map is Blighted or not.
     BlightedMap(bool),
+    /// The map tier of the map.
     MapTier(MapTier),
 }
 
