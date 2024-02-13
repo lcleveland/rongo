f0dfed95d0a3d0d97893990b064cdfbe90cfc790
Lyle Cleveland <lyle.cleveland@yahoo.com>
2024-02-13 13:12:49 -0600

    doc tests

--- libfilter/src/line/influence.rs
+++ libfilter/src/line/influence.rs
@@ -4,18 +4,73 @@ use std::fmt;
 /// Influence
 pub enum Influence {
     /// Shaper
+    ///
+    /// Example:
+    /// ```
+    /// # use libfilter::Influence;
+    /// let influence = Influence::Shaper;
+    /// # assert_eq!(influence.to_string(), "Shaper");
+    /// ```
     Shaper,
+
     /// Elder
+    ///
+    /// Example:
+    /// ```
+    /// # use libfilter::Influence;
+    /// let influence = Influence::Elder;
+    /// # assert_eq!(influence.to_string(), "Elder");
+    /// ```
     Elder,
+
     /// Crusader
+    ///
+    /// Example:
+    /// ```
+    /// # use libfilter::Influence;
+    /// let influence = Influence::Crusader;
+    /// # assert_eq!(influence.to_string(), "Crusader");
+    /// ```
     Crusader,
+
     /// Hunter
+    ///
+    /// Example:
+    /// ```
+    /// # use libfilter::Influence;
+    /// let influence = Influence::Hunter;
+    /// # assert_eq!(influence.to_string(), "Hunter");
+    /// ```
     Hunter,
+
     /// Redeemer
+    ///
+    /// Example:
+    /// ```
+    /// # use libfilter::Influence;
+    /// let influence = Influence::Redeemer;
+    /// # assert_eq!(influence.to_string(), "Redeemer");
+    /// ```
     Redeemer,
+
     /// Warlord
+    ///
+    /// Example:
+    /// ```
+    /// # use libfilter::Influence;
+    /// let influence = Influence::Warlord;
+    /// # assert_eq!(influence.to_string(), "Warlord");
+    /// ```
     Warlord,
+
     /// None
+    ///
+    /// Example:
+    /// ```
+    /// # use libfilter::Influence;
+    /// let influence = Influence::None;
+    /// # assert_eq!(influence.to_string(), "None");
+    /// ```
     None,
 }
 
